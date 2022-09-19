a059a9e1f2f209b8794d9a29a7cf6d7ddba87166
Martin Padron <martinepadronv@gmail.com>
2022-09-19 11:55:17 -0400

    Inicializando Sized

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SIZED</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>piporopo pii</p>
+    </>
+  )
+}
