5d6ddc743eb9c10f942cf4ac6416a85d58819e7e
Martin Padron <martinepadronv@gmail.com>
2022-09-21 09:36:43 -0400

    Optimizando descripciones

--- components/franelas/Franelas.jsx
+++ components/franelas/Franelas.jsx
@@ -16,49 +16,49 @@ const Franelas = () => {
   return (
     <section className={style.section}>
       <ItemLeft
-        modelo="Sized Sighless"
+        modelo="Franela oversize Sized Sighless"
         precio="$19.99"
-        descripcion="Descripcion de las franelas orientadas al SEO"
+        descripcion="Este es nuestro modelo favorito. Un diseño minimalista en clásico negro hacen de esta franela una de nuestras más vendidas."
         foto1={calavera}
         foto2={calavera_2}
-        alt="sized calavera"
+        alt="franela oversize sized sightless"
         enlace="https://api.whatsapp.com/send?phone=+584244429353&text=Hola%20buenos%20d%C3%ADas%20me%20gustar%C3%ADa%20comprar%20la%20franela%20SIZED%20SIGHLESS"
       />
 
       <ItemRight
-        modelo="Sized unlucky cats"
-        precio="$17.99"
-        descripcion="franela unlucky cats"
+        modelo="Franela oversize UNLUCKY CATS model"
+        precio="$16.99"
+        descripcion="¿Eres fan de las caricaturas clásicas? nosotros sí. Con esta prenda llevarás toda la escencia de la vieja escuela a dónde quiera que vayas."
         foto1={cats}
         foto2={cats_item}
-        alt="unlucky cats sized"
+        alt="sized unlucky cats"
         enlace="https://api.whatsapp.com/send?phone=+584244429353&text=Buenos%20d%C3%ADas%20me%20gustar%C3%ADa%20comprar%20una%20franela%20SIZED%20UNLUCKY%20CATS%20"
       />
 
       <ItemLeft
-        modelo="Sized flower fire model"
+        modelo="Camiseta oversize Sized Fire Flower"
         precio="$17.99"
-        descripcion="Descripcion de las franelas orientadas al SEO"
+        descripcion="Lleva el control de las tendencias con este diseño único y original. FIRE FLOWER es para aquellos apasionados que nunca pierden el estilo dónde sea que estén."
         foto1={flower}
         foto2={flower_2}
-        alt="sized flower model"
+        alt="fire flower sized"
         enlace="https://api.whatsapp.com/send?phone=+584244429353&text=Buenos%20d%C3%ADas%20me%20gustar%C3%ADa%20comprar%20una%20franela%20SIZED%20FLOWER%20"
       />
 
       <ItemRight
-        modelo="Sized RED model S"
+        modelo="Franela oversize Sized Mars Red"
         precio="$17.99"
-        descripcion="franela red model red hot chilli"
+        descripcion="¿Quieres destacar? indudablemente nuestro rojo intenso MARS RED es lo que necesitas, acostumbráte a ser el centro de atención cuando lleves esta prenda."
         foto1={red}
         foto2={red2}
-        alt="red chulli uuuu sized"
+        alt="sized mars red model"
         enlace="https://api.whatsapp.com/send?phone=+584244429353&text=Buenos%20d%C3%ADas%20me%20gustar%C3%ADa%20comprar%20una%20franela%20SIZED%20MARS%20RED"
       />
 
       <ItemLeft
-        modelo="Sized Savage Model Exclusive"
-        precio="$17.99"
-        descripcion="Descripcion de las franelas orientadas al SEO"
+        modelo="SAVAGE MOOD sized oversized"
+        precio="$16.99"
+        descripcion="Simple, fresco y minimalista. Tres palabras que describen a la perfección nuestro primer diseño, el bastión de todos los demás."
         foto1={savage}
         foto2={savage2}
         alt="sized savage model"
